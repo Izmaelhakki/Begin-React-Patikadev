c80370384c3fd0480ae8fa1140144c10f3e097d0
Izmaelhakki <100159172+Izmaelhakki@users.noreply.github.com>
2022-12-26 16:59:52 +0300

    Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
-import Header from './components/Header';
+
+import User from './components/User';
+
+const friends = [{ id: 1, name: "kaan" }, { id: 2, name: "yekta" }, { id: 3, name: "ümit" }];
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Header />
-      <Header />
-    </div>
+    <>
+      <User name="İha"
+        surname={"Asmaz"}
+        isLoggedIn={true}
+        age={"31"}
+        friends={friends} 
+        address={{
+          title:'Karabük/Safranbolu',
+          zip:78600
+        }}
+        />
+    </>
   );
 }
 
